8ff6c3f3e4d1929bb0881f3372347b2371efe0ec
AndroidDeveloperLB <liran@android.co.il>
2014-01-06 21:40:17 +0200

    separated library from sample code, removed junk from library and minimized its size.

---
+++ sample/src/com/example/jnibitmapoperationstest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.jnibitmapoperationstest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.jni.bitmap_operations.JniBitmapHolder;
+
+/** just a demo activity to show some of the features of the library */
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class MainActivity extends Activity
+  {
+  private static final int IMAGE_RESID_TO_TEST =R.drawable.test;
+  JniBitmapHolder          bitmapHolder        =new JniBitmapHolder();
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState)
+    {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    //
+    // original
+    //
+    final ImageView imageViewOriginal=(ImageView)findViewById(R.id.imageViewOriginal);
+    final Bitmap b=BitmapFactory.decodeResource(getResources(),IMAGE_RESID_TO_TEST);
+    imageViewOriginal.setImageBitmap(b);
+    //
+    // rotated 90 degress CCW
+    //
+    final ImageView imageViewRotated90degressCcw=(ImageView)findViewById(R.id.imageViewRotated90degressCcw);
+    bitmapHolder.storeBitmap(b);
+    bitmapHolder.rotateBitmapCcw90();
+    imageViewRotated90degressCcw.setImageBitmap(bitmapHolder.getBitmapAndFree());
+    //
+    // rotated 90 degress CW
+    //
+    final ImageView imageViewRotated90degressCw=(ImageView)findViewById(R.id.imageViewRotated90degressCw);
+    bitmapHolder.storeBitmap(b);
+    bitmapHolder.rotateBitmapCw90();
+    imageViewRotated90degressCw.setImageBitmap(bitmapHolder.getBitmapAndFree());
+    //
+    // cropped
+    //
+    final ImageView imageViewCropped=(ImageView)findViewById(R.id.imageViewCropped);
+    bitmapHolder.storeBitmap(b);
+    bitmapHolder.cropBitmap(b.getWidth()/4,b.getHeight()/4,b.getWidth()*3/4,b.getHeight()*3/4);
+    imageViewCropped.setImageBitmap(bitmapHolder.getBitmapAndFree());
+    //
+    // scaled
+    //
+    final ImageView imageViewScaled=(ImageView)findViewById(R.id.imageViewScaled);
+    // final Bitmap b2=resize(b,b.getWidth()/2,b.getHeight()*2);
+    bitmapHolder.storeBitmap(b);
+    bitmapHolder.scaleBitmap(b.getWidth()*2,b.getHeight());
+    imageViewScaled.setImageBitmap(bitmapHolder.getBitmapAndFree());
+    // imageViewScaled.setImageBitmap(b2);
+    }
+  }
